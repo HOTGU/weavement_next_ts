a37573f279652aba633bccfa17848bcb46570172
hotgu <gksrn42@gmail.com>
2024-03-27 18:22:38 +0900

    next config swcminify add

--- next.config.js
+++ next.config.js
@@ -1,20 +1,14 @@
-import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
       "weavement.s3.ap-northeast-2.amazonaws.com",
       "weavement-test.s3.ap-northeast-2.amazonaws.com",
     ],
     minimumCacheTTL: 31536000,
+    swcMinify: true,
   },
   output: "standalone",
-
-  swcMinify: true,
 };
 
-export default withPlaiceholder(nextConfig);
+module.exports = nextConfig;
