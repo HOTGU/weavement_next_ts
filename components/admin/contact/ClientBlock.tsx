184e9e40a467f521acfd844ee9547a3ea08a4db4
hotgu <gksrn42@gmail.com>
2024-04-03 09:50:18 +0900

    admin contact client 최소 width 설정

--- components/admin/contact/ClientBlock.tsx
+++ components/admin/contact/ClientBlock.tsx
@@ -18,14 +18,16 @@ const ClientBlock = ({ client }: ClientBlockProps) => {
       key={client.name}
       className="flex items-center gap-4 overflow-x-auto whitespace-nowrap py-2 sm:py-0"
     >
-      <span className=" w-24">
+      <span className="min-w-[100px]">
         {client.name}
         {client.position ? ` ${client.position}` : " 님"}
       </span>
-      <span className=" w-44">
+      <span className="min-w-[180px]">
         {client.phone ? `연락처: ${client.phone}` : "연락처 없음"}
       </span>
-      <span>{client.email ? `이메일: ${client.email}` : "이메일 없음"}</span>
+      <span className="min-w-[150px]">
+        {client.email ? `이메일: ${client.email}` : "이메일 없음"}
+      </span>
       <span
         className="cursor-pointer text-sky-500 text-sm hover:font-bold transition"
         onClick={() => updateClientModal.onOpen(client)}
